e059609b16dca4a2409e057d2c72955a5e75a8f7
ducduyn31 <ducduy.ababat@gmail.com>
2022-11-11 09:12:31 +0700

    update employee create date format

--- src/main/kotlin/dev/merge/client/hris/models/EmployeeRequest.kt
+++ src/main/kotlin/dev/merge/client/hris/models/EmployeeRequest.kt
@@ -149,18 +149,18 @@ data class EmployeeRequest (
 
     /* The employee's date of birth. */
     @field:JsonProperty("date_of_birth")
-    @field:JsonFormat(pattern = "uuuu-MM-dd'T'HH:mmXXXXX", timezone = "UTC")
+    @field:JsonFormat(pattern = "yyyy-MM-dd")
     val dateOfBirth: java.time.OffsetDateTime? = null,
 
     /* The date that the employee was hired, usually the day that an offer letter is signed. If an employee has multiple hire dates from previous employments, this represents the most recent hire date. Note: If you're looking for the employee's start date, refer to the start_date field. */
     @field:JsonProperty("hire_date")
-    @field:JsonFormat(pattern = "uuuu-MM-dd'T'HH:mmXXXXX", timezone = "UTC")
+    @field:JsonFormat(pattern = "yyyy-MM-dd")
     @Deprecated(message = "This property is deprecated.")
     val hireDate: java.time.OffsetDateTime? = null,
 
     /* The date that the employee started working. If an employee has multiple start dates from previous employments, this represents the most recent start date. */
     @field:JsonProperty("start_date")
-    @field:JsonFormat(pattern = "uuuu-MM-dd'T'HH:mmXXXXX", timezone = "UTC")
+    @field:JsonFormat(pattern = "yyyy-MM-dd")
     val startDate: java.time.OffsetDateTime? = null,
 
     /* When the third party's employee was created. */
@@ -174,7 +174,7 @@ data class EmployeeRequest (
 
     /* The employee's termination date. */
     @field:JsonProperty("termination_date")
-    @field:JsonFormat(pattern = "uuuu-MM-dd'T'HH:mmXXXXX", timezone = "UTC")
+    @field:JsonFormat(pattern = "yyyy-MM-dd")
     val terminationDate: java.time.OffsetDateTime? = null,
 
     /* The URL of the employee's avatar image. */
